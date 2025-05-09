53b840214e1cd5f846ab475e39abcce96e0b956a
JaymedDLC <jaymedl@uninorte.edu.co>
2025-05-09 09:40:21 -0500

    Refactor employee model schema to set default avatar URL and improve formatting

--- server/src/models/employee.model.js
+++ server/src/models/employee.model.js
@@ -1,15 +1,18 @@
 import mongoose from "mongoose";
 
 const EmployeeSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  position: String,
-  avatarUrl: String,
-  skills: [ // Array of skils (SQL, Java, etc.)
-    {
-    name: String // Skill name
-    value: { type: Number, min: 1, max: 10 }, // Skill value (1-10)
+    name: { type: String, required: true },
+    position: String,
+    avatarUrl: {
+        type: String,
+        default: "https://avatars.dicebear.com/api/bottts/default-avatar.svg"
     },
-  ],
+    skills: [ // Array of skils (SQL, Java, etc.)
+        {
+            name: String, // Skill name
+            value: { type: Number, min: 1, max: 10 }, // Skill value (1-10)
+        },
+    ],
 });
 
 export const Employee = mongoose.model("Employee", EmployeeSchema);
