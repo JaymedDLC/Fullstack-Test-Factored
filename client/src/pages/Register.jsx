f95276fc95002a44156e3b6a954885ae90bcd3b5
Jaymed_DLC <jaymedl@uninorte.edu.co>
2025-05-09 23:11:21 -0500

    Enhance Login and Register pages: update styles for better UI, adjust background colors, and improve layout with rounded corners and shadows

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,33 +2,25 @@ import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen bg-black">
       {/* Lado izquierdo */}
-      <div className="w-1/2 bg-blue-200 flex flex-col justify-center items-center p-10">
+      <div className="w-1/2 flex flex-col justify-center items-center p-10">
         <div className="text-left max-w-xs">
-          <h2 className="text-white text-2xl font-light">
-            Need webdesign for your business?
-          </h2>
-          <h2 className="text-blue-700 text-2xl font-bold mt-2">
-            Design Spacee
-          </h2>
-          <p className="text-white text-sm mt-2">
-            will help you.
-          </p>
+          <h2 className="text-white text-2xl font-light"> Need webdesign for your business?</h2>
+          <h2 className="text-blue-700 text-2xl font-bold mt-2">Design Spacee</h2>
+          <p className="text-white text-sm mt-2">will help you.</p>
         </div>
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Octicons-mark-github.svg/512px-Octicons-mark-github.svg.png"
           alt="Logo"
           className="w-20 mt-10"
         />
-        <p className="text-white text-sm mt-4">
-          figma.com/@designspacee
-        </p>
+        <p className="text-white text-sm mt-4">figma.com/@designspacee</p>
       </div>
 
       {/* Lado derecho */}
-      <div className="w-1/2 flex items-center justify-center bg-white">
-        <div className="w-full max-w-md px-8">
+      <div className="w-1/2 flex items-center justify-center bg-white rounded-l-3xl shadow-xl">
+        <div className="w-full max-w-md px-8 py-10">
           <h2 className="text-2xl font-bold mb-6 text-center">Sign-up</h2>
           <form>
             <input
