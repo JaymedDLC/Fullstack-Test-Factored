8033e93b20cd90883cdc49631c685682775aae33
Jaymed_DLC <jaymedl@uninorte.edu.co>
2025-05-10 12:26:00 -0500

    Refactor Dockerfile for multi-stage build; update nginx configuration and adjust docker-compose for frontend service

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], // o [vue()]
+  server: {
+    host: true, // equivalente a '0.0.0.0'
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true
+    }
+  }
 })
